dbfa6144b06d63f981edf9e4b6eed7c72febec61
Pranav8452 <pranavsurve444@gmail.com>
2026-01-29 23:48:38 +0530

    practice

---
+++ JspidersJAVANEW/src/box8OOPS/OOPs4.java
@@ -0,0 +1,33 @@
+package box8OOPS;
+
+public class OOPs4 {
+
+	static String s1 = ColorName();
+	static int a = 10;
+	static String s = fruitname();
+
+	public static void main(String[] args) {
+
+		System.out.println("main start");
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println("Main end");
+	}
+
+	public static String fruitname() {
+		System.out.println("Method start");
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println("Method End");
+		return "Orange";
+	}
+
+	public static String ColorName() {
+		System.out.println("Method start2");
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println("Method End2");
+		return "RED";
+	}
+
+}
