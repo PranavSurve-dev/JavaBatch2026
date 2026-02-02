737da8790178283c24785135fbf6e982bddb2f7c
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-03 00:39:32 +0530

    practice on static method,static variable &
    non-static method,non-static variable.

--- JspidersJAVANEW/src/box8OOPS/HW1.java
+++ JspidersJAVANEW/src/box8OOPS/HW1.java
@@ -1,13 +1,46 @@
 package box8OOPS;
 
+import box8OOPS.HW2.bio;
+
+class notebook {
+	int a = 20; // non-static global variable
+
+	void study() { // non-static method
+		System.out.println("study done");
+	}
+}
+
+class maths {
+	static int c = 30; // static global variable
+
+	static void fail() { // static method
+		System.out.println("maths done");
+	}
+
+}
+
 public class HW1 {
-	int p = 10;
+	int c = 10;
 
 	void book() {
-		System.out.println("done");
+		System.out.println("HW done");
 	}
 
 	public static void main(String[] args) {
-
+		System.out.println("Main Start");
+		System.out.println(new notebook().a);// non-static variable calling
+		new notebook().study();// non-static method calling
+		System.out.println("---------------------------");
+		System.out.println(maths.c); // static variable calling
+		maths.fail();// static method calling
+		System.out.println("-----------------------");
+		System.out.println(new HW1().c);
+		new HW1().book();
+		System.out.println("-----calling from diff source file & diff class------");
+		System.out.println(new HW2().x);// non-static variable calling
+		new HW2().physics();// non-static method calling
+		System.out.println(bio.y);// static variable calling
+		bio.biology();// static method calling
+		System.out.println("Main End");
 	}
 }
--- JspidersJAVANEW/src/box8OOPS/HW2.java
+++ JspidersJAVANEW/src/box8OOPS/HW2.java
@@ -1,5 +1,17 @@
 package box8OOPS;
 
 public class HW2 {
+	int x = 100;
 
+	void physics() {
+		System.out.println("physics done");
+	}
+
+	static class bio {
+		static int y = 200;
+
+		static void biology() {
+			System.out.println("biology done");
+		}
+	}
 }
