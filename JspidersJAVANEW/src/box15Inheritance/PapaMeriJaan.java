9c3bec99a190069910ea429cf26f4af0fa86c119
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-18 21:16:12 +0530

    Single Inheritance

--- JspidersJAVANEW/src/box15Inheritance/PapaMeriJaan.java
+++ JspidersJAVANEW/src/box15Inheritance/PapaMeriJaan.java
@@ -1,22 +1,27 @@
 package box15Inheritance;
 
 class father {
-	void gold() {
+	void gold() { // parent class , super class
 		System.out.println("10kg Gold");
 	}
 }
 
-class Abhijit extends father {
+class Abhijit extends father { // sub class , child class
 	void ganja() {
 		System.out.println("Ganja Fukna");
 	}
 }
 
 public class PapaMeriJaan {
-	public static void main(String[] args) {
+	public static void main(String[] args) { // sub class , child class - Object
 
 		Abhijit a1 = new Abhijit();
 		a1.gold();
 		a1.ganja();
+		System.out.println("----------------------");
+
+		father f1 = new father(); // parent class , super class - Object
+		f1.gold();
+		// f1.ganja();// CTE
 	}
 }
