0dd58d7a8fdc57532f6b957d33eaa45f523d7192
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-20 20:26:55 +0530

    Implict constructor call

---
+++ JspidersJAVANEW/src/box15Inheritance/TMKOC.java
@@ -0,0 +1,30 @@
+package box15Inheritance;
+
+class champklal {
+	champklal() {// Constructor-1
+		System.out.println("A...Nahane ja");
+	}
+}
+
+class jethalal extends champklal {
+	jethalal() { // Constructor-2
+		System.out.println("B...Good morning babita ji");
+	}
+}
+
+class Tappu extends champklal {
+	Tappu() { // Constructor-3
+		System.out.println("C...inside tappu sena");
+	}
+}
+
+public class TMKOC {
+	public static void main(String[] args) {
+		champklal c1 = new champklal();
+		System.out.println("--------------------");
+		jethalal j1 = new jethalal();
+		System.out.println("--------------------");
+		Tappu t1 = new Tappu();
+
+	}
+}
