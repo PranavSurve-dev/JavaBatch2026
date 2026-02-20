6faf377c6a13ad9b3590e0d1a3f79ae27735d0c6
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-20 11:47:01 +0530

    Aptitude practice

---
+++ JspidersJAVANEW/src/progrmas/PrimeNumber.java
@@ -0,0 +1,45 @@
+package progrmas;
+
+//public class PrimeNumber {
+//	public static void main(String[] args) {
+//
+//		int num = 3;
+//		boolean isPrime = true;
+//
+//		if (num <= 1) {
+//			isPrime = false;
+//		} else {
+//			for (int i = 2; i <= num / 2; i++) {
+//				if (num % i == 0) {
+//					isPrime = false;
+//					break;
+//				}
+//			}
+//		}
+//
+//		if (isPrime)
+//			System.out.println(num + " is Prime Number");
+//		else
+//			System.out.println(num + " is Not Prime Number");
+//	}
+//}
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+
+		int num = 17;
+		int count = 0;
+
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				count++;
+			}
+		}
+
+		if (count == 2) {
+			System.out.println(num + " is Prime Number");
+		} else {
+			System.out.println(num + " is Not Prime Number");
+		}
+	}
+}
